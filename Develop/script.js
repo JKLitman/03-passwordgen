5eab7ecfce71a9b914d42494ffe028f50444aa4c
Justin Litman <jlitman7@gmail.com>
2022-09-16 16:34:36 -0500

    added vars

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,16 @@
 // Assignment Code
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "0123456789";
+var symbol = "!@#$%^&*"
+var characters = 0;
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
