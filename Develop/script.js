372437f04cee5d8983b54ff3976ead4a61fc050f
Justin Litman <jlitman7@gmail.com>
2022-09-17 17:07:29 -0500

    added function for character limits

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,12 @@ var symbol = "!@#$%^&*"
 var characters = 0;
 var generateBtn = document.querySelector("#generate");
 
-
+function total_char() {
+  while (char_num<=7 && char_num<=129){
+    window.alert("Choose between 8 and 128 characters.");
+    char_num=prompt ("Enter number of desired characters here.");
+  }
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
